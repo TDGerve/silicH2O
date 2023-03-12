5b7f96b3e5ed762d3fe79159db61c90ea5e4dd78
TDGerve <thomasvangerve@gmail.com>
2023-03-12 22:11:27 +0100

    Update build.py

--- build.py
+++ build.py
@@ -4,20 +4,6 @@ import sys
 
 import PyInstaller.__main__
 
-r"""
-File "C:\Users\u0123694\miniforge3\envs\py310\lib\site-packages\win32ctypes\pywin32\pywintypes.py", line 37, in pywin32error
-    raise error(exception.winerror, exception.function, exception.strerror)
-win32ctypes.pywin32.pywintypes.error: (110, 'EndUpdateResource', 'The system cannot open the device or file specified')
-
-
-https://stackoverflow.com/questions/57180580/pyinstaller-win32ctypes-pywin32-pywintypes-error-1920-loadlibraryexw-syst
-
-https://stackoverflow.com/questions/57932432/pyinstaller-win32ctypes-pywin32-pywintypes-error-2-loadlibraryexw-the-sys
-
-https://stackoverflow.com/questions/41870727/pyinstaller-adding-data-files
-
-
-"""
 
 name = "SilicH2O"
 theme_folder = pathlib.Path(__file__).parents[0] / "src/theme"
