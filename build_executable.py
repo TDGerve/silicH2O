cbfeebbcde013c12c4ecec7ea116b6a6079246e9
TDGerve <thomasvangerve@gmail.com>
2023-04-07 21:16:51 +0200

    updated build

--- build_executable.py
+++ build_executable.py
@@ -1,31 +1,18 @@
+import os
 import pathlib
 import shutil
+import subprocess
 import sys
 
 import PyInstaller.__main__
 
-name = "SilicH2O"
-theme_folder = pathlib.Path(__file__).parents[0] / "src/theme"
-
-
-if "win" in sys.platform:
+if "win32" in sys.platform:
     sep = ";"
 else:
     sep = ":"
 
-if __name__ == "__main__":
-    PyInstaller.__main__.run(
-        [
-            f"--name={name}",
-            "--onedir",
-            "--clean",
-            f"--add-data={theme_folder}{sep}theme",
-            "--exclude-module=.git",
-            "--log-level=WARN",
-            "--noconsole",
-            "run_silicH2O.py",
-        ]
-    )
+name = "SilicH2O"
+theme_folder = pathlib.Path(__file__).parents[0] / "src/theme"
 
 temp_folder = pathlib.Path(__file__).parents[0] / "src/temp"
 calibration_folder = pathlib.Path(__file__).parents[0] / "src/calibration/"
@@ -43,8 +30,27 @@ data_folders = {
     "examples": examples_folder,
 }
 
-for name, folder in data_folders.items():
-    destination = dist / name
-    shutil.copytree(
-        folder, destination, dirs_exist_ok=True, ignore=shutil.ignore_patterns("*.py")
+
+if __name__ == "__main__":
+
+    PyInstaller.__main__.run(
+        [
+            f"--name={name}",
+            "--onedir",
+            "--clean",
+            f"--add-data={theme_folder}{sep}theme",
+            "--exclude-module=.git",
+            "--log-level=WARN",
+            "--noconsole",
+            "run_silicH2O.py",
+        ]
     )
+
+    for name, folder in data_folders.items():
+        destination = dist / name
+        shutil.copytree(
+            folder,
+            destination,
+            dirs_exist_ok=True,
+            ignore=shutil.ignore_patterns("*.py"),
+        )
