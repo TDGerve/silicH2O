d058b61e023a02beee2f9c1bf2b7cf0a9d5b9c66
TDGerve <thomasvangerve@gmail.com>
2023-04-12 09:34:47 +0200

    updated mac compilation

--- build_executable.py
+++ build_executable.py
@@ -3,14 +3,43 @@ import shutil
 import sys
 
 """
-When bundeling for Mac, pyinstaller will probably finish with a warning that the app could not be signed.
+When bundeling for Mac, you will probably run into signing issues and the app not working on other people's computers
+since apple doesn't trust the app.
 To fix that run these commands (from the project folder) in a terminal
 
 first delete all .DS_Store files in the app bundle
     find dist/silich2o.app -name .DS_Store -delete
 
+OPTIONALLY create your own local certificate:
+    Open Keychain Access
+
+    Go to the menu at the top of the screen and select Keychain Access > Certificate Assistant > Create a Certificate
+
+    In the window that appears change Certificate Type to Code Signing , select Create and note the name of the certificate
+
+Create an entitlements.plist file in the main folder:
+    see: https://haim.dev/posts/2020-08-08-python-macos-app/
+
+
 then sign:
-    codesign -s - --force --all-architectures --timestamp --deep dist/SilicH2O.app
+
+    with certificate and entitlements:
+        codesign -s CERTIFICATE-NAME --timestamp --deep --force --entitlements entitlements.plist  ./dist/silicH2O.app
+
+    -> this may take a few mins
+
+    or without local certificate:
+        codesign -s - --force --all-architectures --timestamp --deep --entitlements entitlements.plist dist/SilicH2O.app
+
+
+The app should now run on your local computer and can be shared with others via usb
+
+
+HOWEVER, When downloading SilicH2O from GitHub or elsewhere on the internet, MacOS puts all files in quarantine and the app will not run.
+Remove all files from the bundle from quarantine with:
+    sudo xattr -r -d com.apple.quarantine ./silicH2O.app
+
+note that you need admin rights to do this.
 """
 
 import PyInstaller.__main__
